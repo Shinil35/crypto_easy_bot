0b765a83fab8c19d8b820126af551d1542c2bdb9
Emilio Cafè Nunes <emilionunes@me.com>
2017-11-29 20:12:04 +0100

    Aggiunto file per le api keys

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# Inserisci qui i dati delle tue API
+# Sono richiesti solo i permessi 'View' e 'Trade'
+
+API_KEY = 'API_KEY_HERE'
+API_SECRET = 'API_SECRET_HERE'
+API_PASSPHRASE = 'API_PASSPHRASE_HERE'
