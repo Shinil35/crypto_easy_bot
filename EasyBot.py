1d2552efc6cad3a71361a75047988d0719bc82cb
Emilio Cafe' Nunes <shinil1135@gmail.com>
2017-11-29 23:44:21 +0100

    Modifiche sostanziali, le varie funzioni sono state provate su Sandbox

--- EasyBot.py
+++ EasyBot.py
@@ -1,7 +1,11 @@
 import gdax
 import datetime
+import os.path
+import pickle
+import math
 
 from time import time
+from api_keys import API_KEY, API_SECRET, API_PASSPHRASE, SANDBOX_API_KEY, SANDBOX_API_SECRET, SANDBOX_API_PASSPHRASE
 
 
 class EasyBot:
@@ -11,12 +15,27 @@ class EasyBot:
     last_avg_time = 0
     last_avg = -1
 
+    buy_attempts = 0
+
+
     def __init__(self, product, period_duration, period_euro_limit, avg_days, avg_duration):
         self.product = product
         self.period_duration = period_duration
         self.period_euro_limit = period_euro_limit
         self.avg_days = avg_days
         self.avg_duration = avg_duration
+
+        # Controllo se ho dati salvati in un file
+        self.file_path = product + ".dat"
+
+        try:
+            if os.path.exists(self.file_path):
+                with (open(self.file_path, "rb")) as file:
+                    self.last_buy_time = pickle.load(file)
+                    print("[", product, "] Last buy time loaded from file", sep='')
+        except:
+            print("[", product, "] Error reading saved file ", self.file_path, sep='')
+            self.last_buy_time = 0
 
     def can_buy(self):
         # Controllo se ho già acquistato ETH in questo periodo
@@ -31,15 +50,16 @@ class EasyBot:
     def get_current_buy_price(self):
         public_client = gdax.PublicClient()
 
-        # Chiede al server i prezzi di acquisto/vendita attuali
+        # Chiede al server i prezzi di acquisto attuali
         current_prices = public_client.get_product_order_book(self.product)
 
         ask_price = current_prices['asks'][0][0]
 
-        return ask_price
+        return float(ask_price)
 
     def calculate_avg(self):
 
+        # Utilizzo un meccanismo di cache della media settimanale
         if self.last_avg != -1 and self.last_avg_time > time() - self.avg_duration:
             return self.last_avg
 
@@ -67,8 +87,6 @@ class EasyBot:
 
         total_avg = total_avg / total_count
 
-        print("Average price is", total_avg, self.product)
-
         last_avg = total_avg
         last_avg_time = datetime.time()
 
@@ -77,15 +95,67 @@ class EasyBot:
     def buy_product(self, price):
 
         if not self.can_buy():
-            return False
+            return -1
 
         if price <= 0:
             raise RuntimeError('Cannot buy for a negative price')
 
-        # TODO: Buy product
+        #auth_client = gdax.AuthenticatedClient(SANDBOX_API_KEY, SANDBOX_API_SECRET, SANDBOX_API_PASSPHRASE, api_url="https://api-public.sandbox.gdax.com")
+        auth_client = gdax.AuthenticatedClient(API_KEY, API_SECRET, API_PASSPHRASE)
 
-        return True
+        amount = round(self.period_euro_limit / price, 8)
+        price = round(price, 8)
+
+        resp = auth_client.buy(price=price, size=amount, product_id=self.product)
+
+        print(resp)
+
+        if 'id' in resp:
+            # Ordine piazzato con successo
+            # TODO: Verificare se l'ordine verrà mai completato
+
+            print("[", self.product, "] Buy order placed, id: ",resp['id'] ,", status: ", resp['status'], sep='')
+
+            self.last_buy_time = time()
+
+            try:
+                with (open(self.file_path, "wb")) as file:
+                    pickle.dump(self.last_buy_time, file)
+            except:
+                print("[", self.product, "] Error saving file", self.file_path, sep='')
+
+            return True
+
+        return False
 
     def check_market_and_buy(self):
 
-        pass
+        if not self.can_buy():
+            return False
+
+        # Controllo se il prezzo attuale è inferiore alla media degli ultimi 7 giorni
+
+        avg = self.calculate_avg()
+        cur_price = self.get_current_buy_price()
+
+        print_avg = avg
+        print_cur_price = cur_price
+
+        format(print_avg, '.2f')
+        format(print_cur_price, '.2f')
+
+        print("[", self.product, "] ", self.avg_days, " days average price is ", print_avg, ", current buy price is ", print_cur_price, sep='')
+
+        if cur_price < avg:
+
+            print("[", self.product, "] Placing buy order..", sep='')
+
+            # Compro il product
+
+            bought = self.buy_product(cur_price)
+
+            if bought:
+                print("[", self.product, "] Bought ", self.product, sep='')
+            else:
+                print("[", self.product, "] Attempt to place a buy order failed", sep='')
+                self.buy_attempts += 1
--- bot.py
+++ bot.py
@@ -4,15 +4,24 @@ from EasyBot import EasyBot
 # Impostazioni
 product = 'ETH-EUR'
 
-period_duration = 24 * 60 * 60  # Tempo minimo fra due transazioni, 1 giorno
-period_euro_limit = 2  # Massimi euro investiti nel periodo impostato
+# 4€ ogni 48h, causa del limite minimo di Trading
+# Sarà abbassato il 4 dic 2017 a 0.001, mentre attualmente è a 0.01, circa 3,7€ al prezzo attuale
 
-avg_days = 7  # Su quanti giorni calcolare la media su cui basare gli acquisti
-avg_duration = 2 * 60 * 60  # Validità della media effettuata in precedenza
+period_duration = 48 * 60 * 60  # Tempo minimo fra due transazioni, 1 giorno
+period_euro_limit =  4          # Massimi euro investiti nel periodo impostato
+
+avg_days = 7                    # Su quanti giorni calcolare la media su cui basare gli acquisti
+avg_duration = 2 * 60 * 60      # Validità della media effettuata in precedenza
+
+loop_time = 60                  # Tempo di ciclo del while, min 60
 
 # Main
 bot = EasyBot(product, period_duration, period_euro_limit, avg_days, avg_duration)
 
 while True:
     bot.check_market_and_buy()
-    sleep(5)
+
+    if loop_time < 60:
+        sleep(60)
+    else:
+        sleep(loop_time)
